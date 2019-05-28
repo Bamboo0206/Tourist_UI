d92587f92cf16097e4379512544e166febab11b9
StupidRabbit29 <2922282053@qq.com>
2019-05-28 11:26:44 +0800

    update

--- TouristUI/main.cpp
+++ TouristUI/main.cpp
@@ -9,6 +9,21 @@ bool Quit = false;
 extern SYSTEM_TIME System_Time;
 extern MainWindow *MW;
 
+unsigned __stdcall window_thread(void* pArguments)
+{
+    MainWindow w;
+    //MW指针用于关闭mainwindow
+
+    w.initCoordinate();
+    w.show();
+    cout<<"MainWindow showed"<<endl;
+    MW = &w;
+
+
+    _endthreadex(0);
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -21,28 +36,24 @@ int main(int argc, char *argv[])
     cout<<"创建mouse_thread线程和time_thread线程"<<endl;
 
     //创建mouse_thread线程和time_thread线程
-    //HANDLE Time_Thread = (HANDLE)_beginthreadex(NULL, 0, time_thread, NULL, 0, NULL);
+    HANDLE Time_Thread = (HANDLE)_beginthreadex(NULL, 0, time_thread, NULL, 0, NULL);
     //命令行版本
-    //HANDLE mouse_thread = (HANDLE)_beginthreadex(NULL, 0, mouse_thread, NULL, 0, NULL);
+    HANDLE mouse_thread = (HANDLE)_beginthreadex(NULL, 0, window_thread, NULL, 0, NULL);
 
     cout<<"创建mouse_thread线程和time_thread线程完成"<<endl;
 
 
-    MainWindow w;
-    //MW指针用于关闭mainwindow
 
-    w.initCoordinate();
-    w.show();
-    cout<<"MainWindow showed"<<endl;
-    MW = &w;
 
 
     //等待线程关闭信号，退出线程
-    /*WaitForSingleObject(mouse_thread, MY_INFINITE);
-    CloseHandle(mouse_thread);*/
+    WaitForSingleObject(mouse_thread, MY_INFINITE);
+    CloseHandle(mouse_thread);
     //等待线程关闭信号，退出线程
-    /*WaitForSingleObject(Time_Thread, MY_INFINITE);
-    CloseHandle(Time_Thread);*/
+    WaitForSingleObject(Time_Thread, MY_INFINITE);
+    CloseHandle(Time_Thread);
 
     return a.exec();
 }
+
+
